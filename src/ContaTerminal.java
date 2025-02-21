00eb6d3930d0291ff13017add3e352de69742ee8
viniciusfernandoss <vinicius.senac.ads@gmail.com>
2025-02-21 16:17:52 -0300

    Projeto finalizado

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Olá, Mundo!");
+        //TO DO: conhecer e importar a classe Scanner
+        //Exibit as mensagens para o nosso usuário
+        //Obter pela classe Scanner os valores digitados no terminaal
+        //Exibir a mensagem final
+
+        Scanner leia = new Scanner(System.in);
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.print("Por favor, insira o numero: ");
+        numero = leia.nextInt();
+        System.out.print("Por favor, insira o agencia: ");
+        agencia = leia.next();
+        System.out.print("Por favor, insira o nome: ");
+        nomeCliente = leia.next();
+        System.out.print("Por favor, insira o saldo: ");
+        saldo = leia.nextDouble();
+
+        System.out.println("Olá "+ nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia + ", conta "+ numero +" e seu saldo "+ saldo +" já está disponível para saque");
     }
 }
